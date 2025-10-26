e664553c50aab1a688aaa979316f5372bb0590f1
AlphaBs <ksi123456ab@naver.com>
2025-10-26 22:55:23 +0900

    feat: 자동차 이름 5자 제한 규칙 추가

--- src/main/java/racingcar/RacingCar.java
+++ src/main/java/racingcar/RacingCar.java
@@ -5,6 +5,8 @@ import racingcar.moving.MovingStrategy;
 import racingcar.moving.RandomlyMovingStrategy;
 
 public class RacingCar {
+    private static final int MAX_CAR_NAME = 5;
+
     public static RacingCar createRandomlyMovingCar(String name) {
         return new RacingCar(name, new RandomlyMovingStrategy());
     }
@@ -18,8 +20,12 @@ public class RacingCar {
     private int position;
 
     public RacingCar(String name, MovingStrategy movingStrategy) {
-        this.movingStrategy = movingStrategy;
         this.name = name;
+        if (name.length() > MAX_CAR_NAME) {
+            throw new IllegalArgumentException("자동차 이름의 길이는 " + MAX_CAR_NAME + "자 까지만 설정할 수 있습니다.");
+        }
+
+        this.movingStrategy = movingStrategy;
         this.position = 0;
     }
 
